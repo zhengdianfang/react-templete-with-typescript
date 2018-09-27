60a6b33e0cd3d01cf05e039586c8f06344b06645
zhengdianfang <dfgzheng@thoughtworks.com>
2018-09-27 15:14:29 +0800

    add proxy on dev envoriment

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -4,6 +4,7 @@ const autoprefixer = require('autoprefixer')
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 const InterpolateHtmlPlugin = require('react-dev-utils/InterpolateHtmlPlugin')
 const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin')
 const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin')
@@ -247,6 +248,7 @@ module.exports = {
     // https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
     // You can remove this if you don't use Moment.js:
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    new UglifyJsPlugin(),
   ],
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
--- src/common/request.ts
+++ src/common/request.ts
@@ -1,16 +1,25 @@
 import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
-import { merge } from 'lodash';
+import { each, isEmpty, merge } from 'lodash';
 import { Store } from 'redux';
 import { updateErrorMessage } from '../features/common/redux/actions';
 
 const DEFAULT_REQUEST_CONFIG: AxiosRequestConfig = {
   timeout: 10000,
   responseType: 'application/json',
-  baseURL: process.env.REACT_APP_API_HOST,
+  // baseURL: process.env.REACT_APP_API_HOST,
 };
 
 export const get = (url: string, params: any, headers: any = null) =>
-  axios.get(url, merge(DEFAULT_REQUEST_CONFIG, { headers }));
+  axios.get(generateUrl(url, params), merge(DEFAULT_REQUEST_CONFIG, { headers }));
+
+export const post = (url: string, data: any, headers: any = null) =>
+  axios.post(url, data, merge(DEFAULT_REQUEST_CONFIG, { headers }));
+
+export const remove = (url: string, headers: any = null) =>
+  axios.delete(url, merge(DEFAULT_REQUEST_CONFIG, { headers }));
+
+export const put = (url: string, data: any, headers: any = null) =>
+  axios.put(url, data, merge(DEFAULT_REQUEST_CONFIG, { headers }));
 
 export const setResponseInterceptors = (store: Store) => {
   axios.interceptors.response.use(
@@ -19,4 +28,16 @@ export const setResponseInterceptors = (store: Store) => {
       store.dispatch(updateErrorMessage(error.message));
     },
   );
+};
+
+const generateUrl = (url: string, queryParams: any) => {
+  const params: any[] = [];
+  each(queryParams, (value: any, key: string) => {
+    if (value) {
+      params.push(`${key}=${value}`);
+    }
+  });
+
+  const paramsStr = isEmpty(params) ? '' : '?' + params.join('&');
+  return `${url}${paramsStr}`;
 };
